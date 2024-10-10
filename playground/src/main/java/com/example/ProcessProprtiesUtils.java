a7eb023f09ba8ef9fca7f36f9478401cbc70392d
Kula Somagattu <kulareddy@gmail.com>
2024-10-09 23:48:33 -0500

    dynamic sla

--- playground/src/main/java/com/example/ProcessProprtiesUtils.java
+++ playground/src/main/java/com/example/ProcessProprtiesUtils.java
@@ -8,6 +8,7 @@ import javax.enterprise.context.ApplicationScoped;
 import org.jbpm.workflow.instance.impl.WorkflowProcessInstanceImpl;
 import org.kie.kogito.internal.process.runtime.KogitoProcessContext;
 import java.util.Map;
+import java.util.ArrayList;
 
 import java.util.Date;
 
@@ -22,16 +23,13 @@ public class ProcessProprtiesUtils {
     private static final Logger LOGGER = Logger.getLogger(ProcessProprtiesUtils.class.getName());
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 
-    public void setSLA(String slaDateString, KogitoProcessContext context) {
+    public void setSLA(KogitoProcessContext context) {
         LOGGER.log( Level.INFO, "Process Instance Id is {0}", context.getProcessInstance().getStringId() );
         WorkflowProcessInstanceImpl kogitoProcessInstance = (WorkflowProcessInstanceImpl)context.getProcessInstance();
-        Map<String, Object> metadata = kogitoProcessInstance.getMetaData();
-        metadata.forEach((key,value) -> {
-            LOGGER.log( Level.INFO, "Metadate Key : {0}", key );
-        });
-        kogitoProcessInstance.internalSetSlaDueDate(parseSLADate(slaDateString));
+        // kogitoProcessInstance.internalSetSlaDueDate(parseSLADate(slaDateString));
+        // kogitoProcessInstance.configureSLATimer("#{now() + slaDays}");
         LOGGER.log( Level.INFO, "SLA Due Date: {0}", kogitoProcessInstance.getSlaDueDate() );
-        LOGGER.log( Level.INFO, "SLA Compliance: {0}", kogitoProcessInstance.getSlaCompliance() );
+        LOGGER.log( Level.INFO, "SLA Compliance: {0}", kogitoProcessInstance.getSlaCompliance() );        
         
     }
 
